e405fe6cbba78e872b3f9bc790d710bd3a3188ad
Brian Pereira <69544304+shadow1335@users.noreply.github.com>
2025-02-05 12:37:57 -0500

    Update CodeDemo.cpp

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -5,7 +5,11 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    std::string name;
+    std::cout << "Enter your name: " << std::flush;
+    std::cin >> name;
+    std::cout << "Nice to meet you, " << name << "!" << std::endl;
+    
 
     std::cout << std::endl << std::endl;
     return 0;
