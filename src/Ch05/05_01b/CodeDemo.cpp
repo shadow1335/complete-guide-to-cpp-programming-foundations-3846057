b55d7715120a32305ae5af55e9d4ca189876071f
Brian Pereira <69544304+shadow1335@users.noreply.github.com>
2025-02-06 11:53:02 -0500

    Update CodeDemo.cpp

--- src/Ch05/05_01b/CodeDemo.cpp
+++ src/Ch05/05_01b/CodeDemo.cpp
@@ -8,6 +8,25 @@ int main(){
     int score = 1023;
     bool gamePaused = false;
     char action = 'x';
+    if (score > 1000)
+    {
+        std::cout << "Your score is over 1000. " << std::endl;
+    }
+    if (score % 2 == 0)
+    {
+         std::cout << "Score is even" << std::endl;
+    }
+    else
+    {
+         std::cout << "Score is old" << std::endl;
+    }
+
+    std::cout << "The action '" << action << "'is ";
+    if(action  != 'a' && action != 'w' && action != 'd' && action != 's')
+    {
+        std::cout << "not";
+    }
+    std::cout << "a movement key." << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
